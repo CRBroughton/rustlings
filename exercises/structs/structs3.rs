12cc3044e582ba0a9588f1398378a144b4b6618b
CRBroughton <crbroughton@posteo.uk>
2023-03-29 19:24:13 +0100

    feat: :sparkles: complete structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,13 +4,20 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
     recipient_country: String,
     weight_in_grams: i32,
+}
+
+impl Package {
+    fn different_country(&self) -> bool {
+        if self.recipient_country != self.sender_country {
+            return true;
+        }
+        return false;
+    }
 }
 
 impl Package {
@@ -26,12 +33,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.different_country()
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        return *&self.weight_in_grams * cents_per_gram;
     }
 }
 
